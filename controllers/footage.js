5de2606b839d1473f8d89457ac28b9cf352e2498
peteryu54089 <peteryu54089@gmail.com>
2021-02-03 15:43:54 +0800

    add footage black

---
+++ controllers/footage.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const footage = {};
+
+footage.black = async(ctx) => {
+    await ctx.render('footage-black', {
+        
+    }) 
+};
+
+module.exports = footage;
+
--- routes/public.js
+++ routes/public.js
@@ -3,11 +3,13 @@
 const Router = require('koa-router');
 const index = require('../controllers/index');
 const db = require('../controllers/db');
+const footage = require('../controllers/footage');
 
 const router = new Router();
 
 router.get('/', index);
 router.get('/db', db);
+router.get('/footage-black', footage.black);
 
 module.exports = router;
 
